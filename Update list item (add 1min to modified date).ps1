345c5e9be4bd33c54d58d658e103c570c391bfb4
tb0111 <tb0111@users.noreply.github.com>
2018-09-19 10:54:24 +0100

    New script

---
+++ Update list item (add 1min to modified date).ps1
@@ -0,0 +1,18 @@
+﻿Install-Module SharePointPnPPowerShellOnline
+$url = Read-Host "Enter List Site URL"
+$ListName = Read-Host "Enter List Name" 
+Connect-PnPOnline –Url $url –Credentials (Get-Credential)
+ 
+$items = (Get-PnPListItem -List 'Company Absence Calendar' -PageSize 5000 -Fields "ID").FieldValues 
+ 
+foreach($item in $items)
+{
+  
+Write-Host $item["ID"]
+Write-Host $item["Created"]
+$item.Created = $item["Created"].AddMinutes(1)
+Set-PnPListItem -List 'Company Absence Calendar'-Identity $item.ID -Values @{"Created" = $item.Created}
+}
+
+ 
+  
