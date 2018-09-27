8e3bb4f6b473d649a9cd729eb8c666fa55491715
tb0111 <tb0111@users.noreply.github.com>
2018-09-27 16:54:59 +0100

    Add files via upload

---
+++ Set Welcome Page.ps1
@@ -0,0 +1,15 @@
+﻿#Connect to the SharePoint Online Site
+$Username = Read-Host -Prompt "Enter User"
+$Password = Read-Host -Prompt "Please enter your password" -AsSecureString
+$Site = Read-Host -Prompt "Enter Site URL"
+$Context = New-Object Microsoft.SharePoint.Client.ClientContext($Site)
+$Creds = New-Object Microsoft.SharePoint.Client.SharePointOnlineCredentials($Username,$Password)
+$Context.Credentials = $Creds
+Write-Host "Connected" -ForegroundColor Green
+$Welcome = "Enter New Welcome Page URL" 
+#Update the welcome page
+$web = $Context.Web
+$rootFolder = $web.RootFolder; 
+$rootFolder.WelcomePage = $Welcome;
+$rootFolder.Update();
+$Context.ExecuteQuery();
