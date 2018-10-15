135a6f990808f72b970b3bb99f47f4446f107dd7
tb0111 <tb0111@users.noreply.github.com>
2018-10-15 09:31:58 +0100

    Add files via upload

---
+++ Fully remove all deleted sites.ps1
@@ -0,0 +1,27 @@
+﻿Add-Type –Path "C:\Program Files\Common Files\microsoft shared\Web Server Extensions\16\ISAPI\Microsoft.SharePoint.Client.dll" 
+Add-Type –Path "C:\Program Files\Common Files\microsoft shared\Web Server Extensions\16\ISAPI\Microsoft.SharePoint.Client.Runtime.dll" 
+
+$AdminUrl = Read-Host "Enter Admin URL"
+
+#Get Sites
+Connect-SPOService -Url $AdminUrl
+$sites = Get-SPODeletedSite 
+
+#for each object in $sites, delete
+foreach ($site in $sites)
+{
+
+
+try {
+Remove-SPODeletedSite -Identity $site.URL -Confirm:$false
+Write-Host "$($site.URL) was deleted"
+}
+
+
+catch {
+Write-Host "Error" -ForegroundColor Red
+}
+
+
+}
+
