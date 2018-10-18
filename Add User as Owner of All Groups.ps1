33c87f6f8cac5f317861e31c568fb3c45919019c
tb0111 <tb0111@users.noreply.github.com>
2018-10-18 14:09:00 +0100

    Add files via upload

---
+++ Add User as Owner of All Groups.ps1
@@ -0,0 +1,18 @@
+﻿#Create credential object
+$credObject = Get-Credential
+ 
+#Import the Exchange Online ps session
+$ExchOnlineSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $credObject -Authentication Basic -AllowRedirection
+Import-PSSession $ExchOnlineSession
+
+
+$groups = Get-UnifiedGroup
+
+foreach($Alias in $groups)
+{
+[String]$NewAlias = $Alias
+$NewAlias = $NewAlias.replace(' ','')
+Write-Host $NewAlias
+Add-UnifiedGroupLinks –Identity $NewAlias –LinkType Members –Links [User1],[User2]
+Add-UnifiedGroupLinks –Identity $NewAlias –LinkType Owners –Links [User1],[User2]
+}
